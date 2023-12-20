acc1571f1ace7d8b3e920fd33663ad6ea2a64157
Bomowo <oeccc25@yahoo.com>
2023-12-20 03:33:01 +0200

    Get API, form div with data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,51 @@
-
+import {useState, useEffect} from 'react'
+import Stream from './Stream.jsx'
 import './App.css'
 
 function App() {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+    // streams/OgamingSC2
+    // users/
+    // channels/
+    const url = 'https://twitch-proxy.freecodecamp.rocks/twitch-api/'
+    const startUp = () => {
+      users.forEach(user => makeData(user))
+    }
   
+    const makeData = (user) => {
+      let req1 = (fetch(url + 'streams/' + user))
+      let req2 =(fetch(url + 'channels/' + user))
+  
+        Promise.all([req1, req2])
+            .then(async([resp1, resp2]) => {
+              let data1 = await resp1.json();
+              let data2 = await resp2.json();
+              return [data1, data2]
+            })
+            .then(results => {
+  
+              setData(prevArr => [...prevArr, <Stream 
+                key={results[1]._id} 
+                name={results[1].display_name} 
+                stream={results[0].stream===null?false:true}
+                url={results[1].url}
+                status={results[1].status}
+                />
+              ])
+            })
+  
+    }
+    startUp()
+  },[])
+
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>FCC Twitch API</h1>
+      {data&&data}
     </>
   )
 }
---
+++ src/Stream.jsx
@@ -0,0 +1,8 @@
+export default function Stream ({name, stream, url, status}) {
+    return (
+        <div className={''}>
+            <h2>{name}:</h2>
+            {stream?<p><a href={url} target="_blank" rel="noreferrer">{status}</a></p>:<p><a href={url} target="_blank" rel="noreferrer">offline</a></p>}
+        </div>
+    )
+} 
